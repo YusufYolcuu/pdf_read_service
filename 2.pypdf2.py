c745803f611004d0dc30ec1ea525d292cec84145
Yusuf Yolcu <yyolcu634@gmail.com>
2022-11-30 15:14:51 +0300

    search words and value of it function & edit files

--- 2.pypdf2.py
+++
@@ -1,8 +0,0 @@
-import PyPDF2
-
-sample_pdf = open(r'./PDFS/b5.pdf', mode='rb')
-pdfdoc = PyPDF2.PdfFileReader(sample_pdf)
-
-
-page_one= pdfdoc.getPage(0)
-print(page_one.extractText())
--- 4.tika.py
+++
@@ -1,4 +0,0 @@
-from tika import parser # pip install tika
-
-raw = parser.from_file('./PDFS/b1.pdf')
-print(raw['content'])
